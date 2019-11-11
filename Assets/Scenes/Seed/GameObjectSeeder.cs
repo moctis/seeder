841dfa61702d5139711abe29e28259129aeb7712
sitthidate nanuea <sitthidate@moctis.com>
2019-11-12 03:02:45 +0700

    Test (#6)
    * update
    
    * update ui
    
    * random
    
    * g

---
+++ Assets/Scenes/Seed/GameObjectSeeder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+[Serializable]
+public class GameObjectSeeder : MonoBehaviour
+{
+    public GameObject Original;
+    public Transform Port;
+    public GameObject Target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Instantiate()
+    {
+        Target = Instantiate(Original, Port.position, Port.rotation);
+        Target.transform.localScale = Original.transform.localScale;
+    }
+}
